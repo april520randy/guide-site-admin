12ad136fd2868a0c86468f2e1ab06732a9189c5c
ryder <ryder@kunpeng.company>
2023-05-27 12:14:43 +0800

    优化首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,67 +1,17 @@
 <template>
   <div class="dashboard-container">
-    <el-tabs v-model="activeTab">
-      <el-tab-pane v-for="tab in totalTab" :key="tab.name" :label="tab.label" :name="tab.name">
-        <el-row :gutter="20">
-          <el-col :xs="12" :sm="6" style="padding-top: 10px;">
-            <el-card shadow class="box-card">
-              <div slot="header" class="clearfix">
-                <span>{{ tab.label }}订单数</span>
-              </div>
-              <el-row type="flex" justify="space-between" align="center">
-                <img src="orderCount.png" width="45" height="35">
-                <count-to :start-val="0" :end-val="homeCount[tab.name].orderCount" :duration="2200" class="card-panel-num" />
-              </el-row>
-            </el-card>
-          </el-col>
-          <el-col :xs="12" :sm="6" style="padding-top: 10px;">
-            <el-card shadow class="box-card">
-              <div slot="header" class="clearfix">
-                <span>{{ tab.label }}销售额</span>
-              </div>
-              <el-row type="flex" justify="space-between" align="center">
-                <img src="sale.png" width="45" height="35">
-                <count-to :start-val="0" :decimals="2" :end-val="homeCount[tab.name].saleCount" :duration="2200" class="card-panel-num" />
-              </el-row>
-            </el-card>
-          </el-col>
-          <el-col :xs="12" :sm="6" style="padding-top: 10px;">
-            <el-card shadow class="box-card">
-              <div slot="header" class="clearfix">
-                <span>{{ tab.label }}利润</span>
-              </div>
-              <el-row type="flex" justify="space-between" align="center">
-                <img src="profit.png" width="45" height="35">
-                <count-to :start-val="0" :decimals="2" :end-val="homeCount[tab.name].profit" :duration="2200" class="card-panel-num" />
-              </el-row>
-            </el-card>
-          </el-col>
-          <el-col :xs="12" :sm="6" style="padding-top: 10px;">
-            <el-card shadow class="box-card">
-              <div slot="header" class="clearfix">
-                <span>库存价值</span>
-              </div>
-              <el-row type="flex" justify="space-between" align="center">
-                <img src="stockValue.png" width="45" height="35">
-                <count-to :start-val="0" :decimals="2" :end-val="homeCount.inventoryValue" :duration="2200" class="card-panel-num" />
-              </el-row>
-            </el-card>
-          </el-col>
-        </el-row>
-      </el-tab-pane>
-    </el-tabs>
-
+    <h1>欢迎您</h1>
   </div>
 </template>
 
 <script>
-import CountTo from 'vue-count-to'
+// import CountTo from 'vue-count-to'
 import homeApi from '@/api/home'
 
 export default {
   name: 'Dashboard',
   components: {
-    CountTo
+    // CountTo
   },
   data() {
     return {
